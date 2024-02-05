7d9fad54a020cf19f243a0c9b3f50c09918e572c
erikarispe <79620829+erikarispe@users.noreply.github.com>
2024-02-05 17:13:32 -0600

    Update NBAtodaylivescores.py

--- NBAtodaylivescores.py
+++ NBAtodaylivescores.py
@@ -1,11 +1,7 @@
-#this uses a url from CBS that shows the scoreboard of games for the present day.
-#The output will show the matchup, along with the current scores, and show who is winning/won the game.
-#if the game/games have not started, the scores will have a placeholder of TBD, once they have started the scores will be present.
-
 from bs4 import BeautifulSoup
 import requests
 
-url = 'https://www.cbssports.com/nba/scoreboard/'
+url = 'https://www.cbssports.com/nba/scoreboard/20240204/'
 page = requests.get(url)
 
 soup = BeautifulSoup(page.content, "html.parser")
@@ -24,15 +20,18 @@ for score_card in score_cards:
 
     # Extract scores if available, otherwise set to "TBD"
     scores = score_card.find_all('td', class_='total')
-    score1 = scores[0].text.strip() if scores else "TBD"
-    score2 = scores[1].text.strip() if scores else "TBD"
+    score1text = scores[0].text.strip() if scores else "TBD"
+    score2text = scores[1].text.strip() if scores else "TBD"
 
-    if score1 or score2 == "TBD":
-        winner = "TBD"
-    elif score1 > score2:
+    score1 = int(score1text)
+    score2 = int(score2text)
+
+    if score1 > score2:
         winner = team1
-    else:
+    elif score2 > score1:
         winner = team2
+    else:
+        winner = "TBD"
 
     # Print the results
     print(f"{team1}: {score1}")
