8c29b362f39ada8cf6fe14ada819becc9e5173c4
erikarispe <79620829+erikarispe@users.noreply.github.com>
2024-02-06 15:58:22 -0600

    Update NBAtodaylivescores.py

--- NBAtodaylivescores.py
+++ NBAtodaylivescores.py
@@ -1,40 +1,58 @@
 from bs4 import BeautifulSoup
 import requests
+from flask import Flask, render_template
+from datetime import date
 
-url = 'https://www.cbssports.com/nba/scoreboard/20240204/'
+#http://127.0.0.1:5000/  flask url
+
+app = Flask(__name__)
+
+today = date.today()
+formatted_date = today.strftime('%B %d, %Y')
+
+
+
+url = 'https://www.cbssports.com/nba/scoreboard/'
 page = requests.get(url)
 
 soup = BeautifulSoup(page.content, "html.parser")
 
-# Find all elements with the class "single-score-card"
 score_cards = soup.find_all('div', class_='single-score-card')
+
+# Initialize variables
+teams = []
+scores = []
+winners = []
 
 # Iterate through each score card
 for score_card in score_cards:
     # Extract team names
-    teams = score_card.find_all('a', class_='team-name-link')
-    team1 = teams[0].text.strip()
-    team2 = teams[1].text.strip()
-    
-    
+    teams_data = score_card.find_all('a', class_='team-name-link')
+    team1 = teams_data[0].text.strip()
+    team2 = teams_data[1].text.strip()
 
     # Extract scores if available, otherwise set to "TBD"
-    scores = score_card.find_all('td', class_='total')
-    score1text = scores[0].text.strip() if scores else "TBD"
-    score2text = scores[1].text.strip() if scores else "TBD"
-
-    score1 = int(score1text)
-    score2 = int(score2text)
+    scores_data = score_card.find_all('td', class_='total')
+    score1 = int(scores_data[0].text.strip()) if scores_data else "TBD"
+    score2 = int(scores_data[1].text.strip()) if scores_data else "TBD"
 
     if score1 > score2:
-        winner = team1
+        winner_data = team1
     elif score2 > score1:
-        winner = team2
+        winner_data = team2
     else:
-        winner = "TBD"
+        winner_data = "TBD"
 
-    # Print the results
-    print(f"{team1}: {score1}")
-    print(f"{team2}: {score2}")
-    print("Team in lead/Winner : ",winner)
-    print("\n")
+    # Append data to lists
+    teams.append({'team1': team1, 'team2': team2})
+    scores.append({'score1': score1, 'score2': score2})
+    winners.append(winner_data)
+
+    
+
+# Route to render the template
+@app.route('/')
+def index():
+    return render_template('basketballscores.html', teams=teams, scores=scores, winners=winners, formatted_date=formatted_date)
+if __name__ == '__main__':
+    app.run(debug=True)
